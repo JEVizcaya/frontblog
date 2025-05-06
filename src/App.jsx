bf55887fe3f77ddd2c9da480faabe9740a4cdeec
JEVizcaya <jevizcaya@hotmail.com>
2025-05-07 01:15:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Editor from './Editor';
 import AddPost from './AddPost';
 import EditPost from './EditPost';
 import PostDetail from './PostDetail';
-import { Link } from 'react-router-dom';
 import { API_BASE_URL } from './config';
 
 function App() {
